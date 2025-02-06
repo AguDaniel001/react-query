54ea403a83f4bf1577c4554dadece62d4defee4d
Agudaniel <adofintl@gmail.com>
2025-02-06 19:06:46 +0100

    created-custom-query-hook

---
+++ src/Hooks/useSuperHeroesData.jsx
@@ -0,0 +1,27 @@
+import { useQuery } from "react-query"
+import axios from "axios"
+
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes')
+}
+export const useSuperHeroesData = (onSuccess, onError) => {
+ return useQuery(
+    'super-heroes', fetchSuperHeroes, {
+      // cacheTime: 5000, 
+      // staleTime: 0, 
+      // enabled: true,
+      // refetchOnMount: true,
+      // refetchOnWindowFocus: true,
+      // refetchInterval: false,
+      // refetchIntervalInBackground: false,
+      onSuccess: onSuccess,
+      onError: onError,
+      select: (data) => {
+        return data.data.map((hero) => ({
+          name: hero.name,
+        }))
+        // return superHeroNames
+      }
+    }
+  )
+}
--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -1,10 +1,4 @@
-import { useQuery } from "react-query"
-import axios from "axios"
-
-
-const fetchSuperHeroes = () => {
-  return axios.get('http://localhost:4000/superheroes')
-}
+import { useSuperHeroesData } from "../Hooks/useSuperHeroesData"
 
 export const RQSuperHeroesPage = () => {
   
@@ -16,25 +10,7 @@ export const RQSuperHeroesPage = () => {
     console.log('Perform side effect after encountering error', error)
   }
 
-  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
-    'super-heroes', 
-    fetchSuperHeroes,
-    {
-      // cacheTime: 5000, 
-      // staleTime: 0, 
-      // enabled: true,
-      // refetchOnMount: true,
-      // refetchOnWindowFocus: true,
-      // refetchInterval: false,
-      // refetchIntervalInBackground: false,
-      onSuccess: onSuccess,
-      onError: onError,
-      select: (data) => {
-        const superHeroNames = data.data.map((hero) => hero.name)
-        return superHeroNames
-      }
-    }
-  )
+  const { isLoading, data, isError, error, isFetching, refetch } = useSuperHeroesData(onSuccess, onError)
 
   console.log({isLoading, isFetching})
 
@@ -45,9 +21,7 @@ export const RQSuperHeroesPage = () => {
   if(isError) {
     return <h2>{error.message}</h2>
   }
-
   
-
   return (
     <>
     <h2>RQ Super Heroes Page</h2>
@@ -58,8 +32,8 @@ export const RQSuperHeroesPage = () => {
       // })
 
       //Data Transformation
-      data.map((heroName) => {
-        return <div key={heroName}>{heroName}</div>
+      data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>
       })
     }
     
