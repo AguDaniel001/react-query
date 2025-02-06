099c954849af885d480683ca66f4f5cfcf9b6b07
Agudaniel <adofintl@gmail.com>
2025-02-06 15:44:52 +0100

    react-query-fetch-request

---
+++ src/components/RQSuperHeroes.page.jsx
@@ -0,0 +1,61 @@
+import { useQuery } from "react-query"
+import axios from "axios"
+
+
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes')
+}
+
+export const RQSuperHeroesPage = () => {
+  
+  const onSuccess = (data) => {
+    console.log('Perform side effect after data fetching', data)
+  }
+
+  const onError = (error) => {
+    console.log('Perform side effect after encountering error', error)
+  }
+
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
+    'super-heroes', 
+    fetchSuperHeroes,
+    {
+      // cacheTime: 5000, 
+      // staleTime: 0, 
+      // enabled: true,
+      // refetchOnMount: true,
+      // refetchOnWindowFocus: true,
+      // refetchInterval: false,
+      // refetchIntervalInBackground: false,
+      onSuccess: onSuccess,
+      onError: onError,
+      
+    }
+  )
+
+  console.log({isLoading, isFetching})
+
+  if(isLoading || isFetching) {
+    return <h2>Loading...</h2>
+  }
+
+  if(isError) {
+    return <h2>{error.message}</h2>
+  }
+
+  
+
+  return (
+    <>
+    <h2>RQ Super Heroes Page</h2>
+    <button onClick={refetch}>Refresh</button>
+    {
+      data?.data.map(hero => {
+        return <div key={hero.name}>{hero.name}</div>
+      })
+    }
+    
+    </>
+  )
+  
+}
