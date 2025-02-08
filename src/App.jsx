4bb9639d198ffcfdf813f3a2c63b265b741f8efc
Agudaniel <adofintl@gmail.com>
2025-02-08 05:11:49 +0100

    created-parallel-page-and-route

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { HomePage } from './components/Home.page'
 import { RQSuperHeroesPage } from './components/RQSuperHeroes.page'
 import { SuperHeroesPage } from './components/SuperHeroes.page'
 import { RQSuperHeroPage } from './components/RQSuperHeroPage'
+import { ParallelQueriesPage } from './components/ParallelQueries.page'
 
 
 const queryClient = new QueryClient()
@@ -25,9 +26,13 @@ function App() {
             <li>
               <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
             </li>
+            <li>
+              <Link to='/rq-parallel'>Parallel Queries</Link>
+            </li>
           </ul>
         </nav>
         <Routes>
+          <Route path='/rq-parallel' element={<ParallelQueriesPage />} />
           <Route path='/rq-super-heroes/:heroId' element={<RQSuperHeroPage />} />
           <Route path='/super-heroes' element={<SuperHeroesPage />} />
           <Route path='/rq-super-heroes' element={<RQSuperHeroesPage />} />
---
+++ src/components/ParallelQueries.page.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const ParallelQueriesPage = () => {
+  return <div>ParallelQueries Page</div>
+}
