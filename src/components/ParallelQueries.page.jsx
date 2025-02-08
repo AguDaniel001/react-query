2fa54226c4146f4687c9656c628bf5298bc6c6d3
Agudaniel <adofintl@gmail.com>
2025-02-08 05:58:24 +0100

    completed-parallel-queries

--- src/components/ParallelQueries.page.jsx
+++ src/components/ParallelQueries.page.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+  return axios.get('http://localhost:4000/superheroes')
+}
+
+const fetchFriends = () => {
+  return axios.get('http://localhost:4000/friends')
+}
 
 export const ParallelQueriesPage = () => {
-  return <div>ParallelQueries Page</div>
+  const {data: superHeroes} = useQuery('super-heroes', fetchSuperHeroes)
+  const {data: friends} = useQuery('friends', fetchFriends)
+  
+  return <div>ParallelQueries Page </div>
 }
