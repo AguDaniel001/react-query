e03e4289c7d467e759dca09c6fa2ea0e98b25905
Agudaniel <adofintl@gmail.com>
2025-02-09 19:02:24 +0100

    added-initial-state-to-useSuperHeroData-hook

--- src/Hooks/useSuperHeroData.jsx
+++ src/Hooks/useSuperHeroData.jsx
@@ -1,12 +1,23 @@
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import axios from "axios";
 
 const fetchSuperHero = async ({ queryKey }) => {
     const heroId = queryKey[1];
-    const response = await axios.get(`http://localhost:4000/superheroes/${heroId}`)
+    const response = await axios.get(`http://localhost:4000/superheroes/${heroId}`);
   return response.data;
 }
-
 export const useSuperHeroData = (heroId) => {
-  return useQuery(['super-hero', heroId], fetchSuperHero);
+  const queryClient = useQueryClient();
+  return useQuery(['super-hero', heroId], fetchSuperHero, {
+    initialData: () => {
+      const heroes = queryClient.getQueryData('super-heroes')?.data;
+      const hero = heroes?.find((hero) => hero.id === parseInt(heroId));
+
+      if (hero) {
+        return hero;
+      } else {
+        return undefined;
+      }
+    },
+  });
 }
--- src/components/DependentQueries.page.jsx
+++ src/components/DependentQueries.page.jsx
@@ -8,17 +8,28 @@ const fetchUserByEmail = (email) => {
 const fetchCoursesByChannelId = (channelId) => {
   return axios.get(`http://localhost:4000/channels/${channelId}`)
 }
+const fetchCourses = () => {
+  return axios.get("http://localhost:4000/channels")
+}
 
-export const DependentQueriesPage = ({ email}) => {
+export const DependentQueriesPage = ({ email }) => {
 
   const { data: user } = useQuery(["user", email], () => 
     fetchUserByEmail(email))
 
   const channelId = user?.data.channelId
+  const userId = user?.data.id
 
   useQuery(['courses', channelId], () => fetchCoursesByChannelId(channelId), {
     enabled: !!channelId,
   })
 
-  return <div>DependentQueries Page</div>
+  return  <div>
+            <h3>DependentQueries Page</h3>
+            <p>User: {userId}</p>
+            <p>Channel Name: {channelId}</p>
+            <p>Courses: {channelId}</p>
+            {/* <p>Courses: {data?.courses}</p> */}
+  </div> 
+  
 }
